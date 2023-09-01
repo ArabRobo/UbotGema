68a0d0aba3ba26e4f832c18dd168ab38f58d246c
Si Arab - <98678420+fadhilabdat04@users.noreply.github.com>
2023-09-01 14:37:55 +0700

    Update bot.py

--- Ubot/modules/bot/bot.py
+++ Ubot/modules/bot/bot.py
@@ -143,65 +143,11 @@ async def close(_, query: CallbackQuery):
     ))
 
 @app.on_callback_query(filters.regex("help_u"))
-async def module_help(client: Client, message: Message):
-    cmd = message.command
-    help_arg = ""
-    bot_username = (await app.get_me()).username
-    if len(cmd) > 1:
-        help_arg = " ".join(cmd[1:])
-    elif not message.reply_to_message and len(cmd) == 1:
-        try:
-            nice = await client.get_inline_bot_results(bot=bot_username, query="helper")
-            await asyncio.gather(
-                message.delete(),
-                client.send_inline_bot_result(
-                    message.chat.id, nice.query_id, nice.results[0].id
-                ),
-            )
-        except BaseException as e:
-            print(f"{e}")
-            ac = PrettyTable()
-            ac.header = False
-            ac.title = "Modules"
-            ac.align = "l"
-            for x in split_list(sorted(CMD_HELP.keys()), 2):
-                ac.add_row([x[0], x[1] if len(x) >= 2 else None])
-            xx = await client.send_message(
-                message.chat.id,
-                f"```{str(ac)}```",
-                reply_to_message_id=ReplyCheck(message),
-            )
-            await xx.reply(
-                f"**Usage:** `help broadcast` **To View Module Information**"
-            )
-            return
+async def module_help(_, callback_query: CallbackQuery):
+class Data:
 
-    if help_arg:
-        if help_arg in CMD_HELP:
-            commands: dict = CMD_HELP[help_arg]
-            this_command = f"**Bantuan {str(help_arg).upper()}**\n\n"
-            for x in commands:
-                this_command += f"๏ **Keterangan:** `{str(commands[x])}`\n\n"
-            this_command += "@Dhilnihnge | Arab-Uprem"
-            await edit_or_reply(
-                message, this_command, parse_mode=enums.ParseMode.MARKDOWN
-            )
-        else:
-            await edit_or_reply(
-                message,
-                f"`{help_arg}` **tidak ada dalam list modul.**",
-            )
-
-
-def add_command_help(module_name, commands):
-    if module_name in CMD_HELP.keys():
-        command_dict = CMD_HELP[module_name]
-    else:
-        command_dict = {}
-
-    for x in commands:
-        for y in x:
-            if y is not x:
-                command_dict[x[0]] = x[1]
-
-    CMD_HELP[module_name] = command_dict
+    text_help_menu = (
+        f"**Help Menu**\n**• Prefixes** : `. ! ?`"
+    )
+    reopen = [[InlineKeyboardButton("Open", callback_data="reopen")]]
+   
